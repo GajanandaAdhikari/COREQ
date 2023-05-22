322ff2e5e3abb14be3926c409745da8f22f7a60d
Hunter-420 <khanalnischal2075@gmail.com>
2023-05-22 11:13:46 +0545

    friend suggestion, top projects, top research articles added on feed

--- src/components/TopResearchArticles.js
+++ src/components/TopResearchArticles.js
@@ -5,35 +5,39 @@ const users=[
     {
         name:"Nischal Khanal", 
         userName:"Hunter420",
-        userImage:"https://avatars.githubusercontent.com/u/92971596?s=120&v=4"
+        userImage:"https://avatars.githubusercontent.com/u/92971596?s=120&v=4",
+        title:"Store one human gene information to another human"
     },
     {
         name:"Ujjwal Dhakal",
         userName:"ujjwalnp",
-        userImage:"https://avatars.githubusercontent.com/u/68050149?s=120&v=4"
+        userImage:"https://avatars.githubusercontent.com/u/68050149?s=120&v=4",
+        title:"Self driving motorcycle"
     },
     {
         name:"Gazananda Mani Adhikari",
         userName:"cosmicGZ",
-        userImage:"https://avatars.githubusercontent.com/u/90420230?s=120&v=4"
+        userImage:"https://avatars.githubusercontent.com/u/90420230?s=120&v=4",
+        title:"Quantum Computing and it's benifits"
     }
 ];
 
-function FriendSuggestionList(){
+function TopResearchArticlesList(){
     return(
         <div>
             {
-                users.map(user=><SuggestedFriend
+                users.map(user=><SuggestedResearchArticles
                     name={user.name}
                     userName={user.userName}
                     userImage={user.userImage}
-                    ></SuggestedFriend>)
+                    projTitle={user.title}
+                    ></SuggestedResearchArticles>)
             }
         </div>
     )
 }
 
-function SuggestedFriend({name,userName,userImage}){
+function SuggestedResearchArticles({name,userName,userImage,projTitle}){
     return(
         <>
         <li className="py-3 sm:py-4 ">
@@ -42,15 +46,16 @@ function SuggestedFriend({name,userName,userImage}){
                         <img className="w-8 h-8 rounded-full" src={userImage}/>
                     </div>
                     <div class="flex-1 min-w-0">
-                        <p className="text-lg sm:font-medium md:font-xl hover:text-sky-700 truncate ">
-                            {name}
+                        <h1 className='antialiased font-semibold hover:text-purple-700 text-lg'>{projTitle}</h1>
+                        <p className="text-sm sm:font-medium md:font-xl  truncate hover:text-sky-700">
+                            {name} <span className="text-sm text-gray-700 truncate dark:text-gray-700">@{userName}</span>
                         </p>
-                        <p className="text-sm text-gray-700 truncate dark:text-gray-700">
+                        {/* <p className="text-sm text-gray-700 truncate dark:text-gray-700">
                             @{userName}
-                        </p>
+                        </p> */}
                     </div>
                     <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                    <Button variant="text">Follow</Button>
+                    <Button variant="text">Visit</Button>
                     </div>
                 </div>
             </li>
@@ -58,18 +63,18 @@ function SuggestedFriend({name,userName,userImage}){
     )
 }
 
-function FriendSuggestion(){
+function TopResearchArticles(){
     return(
         <>
         <div className="flex">
 
 <div class="w-full max-w-md  p-4  border border-gray-200 rounded-lg shadow sm:p-8 ">
     <div class="flex items-center justify-between mb-4">
-        <h5 class="text-xl font-bold leading-none  ">Friend Suggestions</h5>
+        <h5 class="text-xl font-bold leading-none  ">Top Research Articles</h5>
    </div>
    <div class="flow-root">
         <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-            <FriendSuggestionList/>
+            <TopResearchArticlesList/>
                     </ul>
    </div>
 </div>
@@ -78,4 +83,4 @@ function FriendSuggestion(){
     )
 }
 
-export default FriendSuggestion;
+export default TopResearchArticles;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,14 +4,10 @@ import UserName from "../components/UserName";
 import Feed from "../components/Feed";
 function Landing(){
     return(
-        <div className="flex divide-x divide-solid w-full">
+        <div className=" flex divide-x divide-solid w-full">
         <div className="mt- ">
          
-        <div className="mt-30  pl-6 ">
-            <SideBar></SideBar>
-            
-            
-        </div>
+        
         </div>
         <Feed></Feed>
         </div>
