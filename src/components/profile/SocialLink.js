838e157829b8d7630ff4e409ece20a39eb2e19d4
Hunter-420 <khanalnischal2075@gmail.com>
2023-07-10 14:34:54 +0545

    No Twitter username update option in profile/editProfile/socialLinks #46 feature added

--- src/components/profile/SocialLink.js
+++ src/components/profile/SocialLink.js
@@ -4,7 +4,8 @@ import {
   faFacebook,
   faInstagram,
   faGithub,
-  faLinkedin
+  faLinkedin,
+  faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 
 export default function SocialLink() {
@@ -15,10 +16,11 @@ export default function SocialLink() {
           <h3 className="text-2xl font-bold pt-20 pl-20">Social Link</h3>
         </div>
         <div class="w-2/3 mx-auto mb-10 pb-20 pt-20 mt-20">
-          <span className="flex mt-8"> <FontAwesomeIcon icon={faGithub} size="3x" /> <input class="ml-5  justify-center  shadow appearance-none border border-bcolor rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Github Username" /></span>
-          <span className="flex mt-8"> <FontAwesomeIcon icon={faLinkedin} size="3x" /> <input class="ml-7 justify-center  shadow appearance-none border border-bcolor rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Linkedin Username" /></span>
-          <span className="flex mt-8"> <FontAwesomeIcon icon={faFacebook} size="3x" /> <input class="ml-5 justify-center  shadow appearance-none border border-bcolor rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Facebook Username" /></span>
-          <span className="flex mt-8"> <FontAwesomeIcon icon={faInstagram} size="3x" /> <input class="ml-6 justify-center  shadow appearance-none border border-bcolor rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Instagram Username" /></span>
+          <span className="flex mt-8"> <FontAwesomeIcon icon={faGithub} size="3x" /> <input class="ml-5  justify-center  shadow appearance-none border border-bcolor rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="githubUsername" type="text" placeholder="Github Username" /></span>
+          <span className="flex mt-8"> <FontAwesomeIcon icon={faLinkedin} size="3x" /> <input class="ml-7 justify-center  shadow appearance-none border border-bcolor rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="likedinUsername" type="text" placeholder="Linkedin Username" /></span>
+          <span className="flex mt-8"> <FontAwesomeIcon icon={faFacebook} size="3x" /> <input class="ml-5 justify-center  shadow appearance-none border border-bcolor rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="facebookUsername" type="text" placeholder="Facebook Username" /></span>
+          <span className="flex mt-8"> <FontAwesomeIcon icon={faInstagram} size="3x" /> <input class="ml-6 justify-center  shadow appearance-none border border-bcolor rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="instagramUsername" type="text" placeholder="Instagram Username" /></span>
+          <span className="flex mt-8"> <FontAwesomeIcon icon={faTwitter} size="3x" /> <input class="ml-6 justify-center  shadow appearance-none border border-bcolor rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="twitterUsername" type="text" placeholder="Twitter Username" /></span>
 
         </div>
 
