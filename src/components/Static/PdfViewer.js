f7d3c448ceb2b1728ad51c9259eb8a0b94d4dbe3
Hunter-420 <khanalnischal2075@gmail.com>
2023-07-12 07:27:40 +0545

    pdf viewer added

--- src/components/Static/PdfViewer.js
+++ src/components/Static/PdfViewer.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Document, Page } from 'react-pdf';
 
-function PDFViewer({ pdfUrl }) {
+function PDFViewer({ pdfUrl,message }) {
   const handleDownload = () => {
     window.open(pdfUrl, '_blank');
   };
 
   return (
-    <div>
+	<div className="flex justify-center flex-col items-center w-full   mt-5 ">
+    <div className='w-full h-[100px]  flex flex-col items-center px-4 py-6 bg-back-color text-blue rounded-lg shadow-lg  cursor-pointer hover:bg-blue hover:text-white'>
       <Document file={pdfUrl}>
         <Page pageNumber={1} />
       </Document>
-      <button onClick={handleDownload}>Download PDF</button>
+      <button onClick={handleDownload}>{message}</button>
     </div>
+	</div>
   );
 }
 
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -5,7 +5,7 @@ import FriendSuggestion from "../components/suggestion/FriendSuggestion";
 import TopProjects from "../components/suggestion/TopProjects";
 import TopResearchArticles from "../components/suggestion/TopResearchArticles";
 import CirculateProjects from "../components/circulate/CirculateProjects";
-import QueryPostShow from "../components/showpost/QueryPostShow";
+import ProjectShow from "../components/showpost/ProjectShow";
 
 
 function Project() {
@@ -33,7 +33,7 @@ function Project() {
             <div className="2xl:pl-20">
               <CirculateProjects />
             </div>
-            <div className="2xl:pl-20"><QueryPostShow /></div>
+            <div className="2xl:pl-20"><ProjectShow /></div>
           </div>
 
           <div className="max-sm:hidden grid  overflow-y-scroll md:h-[540px] 2xl:h-[760px] ">
