699cdc9fda4732cbf8129dadc91ec08b27c903dc
Hunter-420 <khanalnischal2075@gmail.com>
2023-10-01 01:08:21 +0545

    collaborator spelling fixed

--- src/components/circulate/CirculateProjects.js
+++ src/components/circulate/CirculateProjects.js
@@ -133,7 +133,7 @@ function CirculateProjects() {
         />
         <input
           type="text"
-          id="collaburator"
+          id="collaborator"
           placeholder="Collaburators"
           class="flex mt-4 items-start md:text-[20px] w-full 2xl:h-[40px]  p-1 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           value={collabrators}
